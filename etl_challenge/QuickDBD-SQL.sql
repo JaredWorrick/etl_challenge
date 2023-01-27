4295c3a488ce5bee9d269f3194e6399fd9b491ed
JaredWorrick <115746414+JaredWorrick@users.noreply.github.com>
2023-01-27 00:11:55 -0600

    uploading files for ETA challenge.
    These are all the files to get started for submission.

---
+++ etl_challenge/QuickDBD-SQL.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/2VLWMB
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "category" (
+    "category_id" varchar(5)   NOT NULL,
+    "category" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(10)   NOT NULL,
+    "subcategory" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" INTEGER   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" INTEGER   NOT NULL,
+    "contact_id" INTEGER   NOT NULL,
+    "company_name" varchar   NOT NULL,
+    "description" varchar   NOT NULL,
+    "goal" decimal   NOT NULL,
+    "pledged" decimal   NOT NULL,
+    "outcome" varchar(10)   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
+    "country" char(5)   NOT NULL,
+    "currency" char(10)   NOT NULL,
+    "launched_date" Date   NOT NULL,
+    "end_date" Date   NOT NULL,
+    "category_id" char(10)   NOT NULL,
+    "sub_category_id" char(10)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
+REFERENCES "Campaign" ("category_id");
+
+ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Campaign" ("sub_category_id");
+
+ALTER TABLE "Contacts" ADD CONSTRAINT "fk_Contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Campaign" ("contact_id");
+
